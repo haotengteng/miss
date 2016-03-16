773877553720e4d2ef73248fdb2c07eed6dfca83
haotengteng <1798440444@qq.com>
2016-03-16 19:51:49 +0800

    update

--- md-miss4/src/main/java/com/haoteng/miss4/md/dao/AccountDao.java
+++ md-miss4/src/main/java/com/haoteng/miss4/md/dao/AccountDao.java
@@ -1,10 +1,12 @@
 package com.haoteng.miss4.md.dao;
 
 import com.haoteng.miss4.md.module.Account;
+import org.springframework.stereotype.Repository;
 
 /**
  * Created by Administrator on 2016/3/16.
  */
+@Repository
 public interface AccountDao {
     int addAccount(Account account);
 }
